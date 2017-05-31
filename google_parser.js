c0c12158073df714668ce4689f905c6867cedc88
미쁨(trusty_people) <jioo0224@naver.com>
2017-05-31 19:37:36 +0900

    jpg 검색 추가 등...

--- google_parser.js
+++ google_parser.js
@@ -20,6 +20,26 @@ module.exports.img = (search) => {
         });
     });
 }
+module.exports.jpg = (search) => {
+    return new Promise((resolve, reject) => {
+        request({
+            url: "https://www.google.com/search?q="+encodeURIComponent(search+' -ilbe.com')+"&tbm=isch&ie=UTF-8&tbs=ift:jpg",
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
+            }
+        }, (err, res, body) => {
+            if(err) reject(err);
+            let dom = parser.parseFromString(body);
+            let list = dom.getElementsByClassName('rg_meta');
+            let result = new Array();
+            list.forEach((cur) => {
+                let json = JSON.parse(cur.innerHTML);
+                result.push({img: json.ou, url: json.ru});
+            });
+            resolve(result);
+        });
+    });
+}
 module.exports.search = (search) => {
     return new Promise((resolve, reject) => {
         google(search+' -ilbe.com', (err, res) => {
--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 const google = require('./google_parser.js');
 
-google.search('치탄다 에루')
+google.search('테스트')
 .then((res) => {
     console.log(res);
 }).catch((err) => {
     console.log(err);
 });
-google.img('치탄다 에루')
+google.img('ajdjaivcjeoijvoiajveoijwaoijvoiheaohgiuab iuegaiuvbaiusneaiu')
 .then((res) => {
-    console.log(res);
+    console.log(res[0]);
 }).catch((err) => {
     console.log(err);
 });
