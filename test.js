bd3971d37a2850ca99ca9268ca16cb257306b7de
small_sunshine <jioo0224@naver.com>
2017-06-16 06:43:45 +0900

    계정 변경 적용/테스트 코드 수정

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const google = require('./google_parser.js');
 
-google.search('테스트')
+google.search('Chitanda Eru')
 .then((res) => {
     console.log(res);
 }).catch((err) => {
     console.log(err);
 });
-google.img('ajdjaivcjeoijvoiajveoijwaoijvoiheaohgiuab iuegaiuvbaiusneaiu')
+google.img('Chitanda Eru')
 .then((res) => {
     console.log(res[0]);
 }).catch((err) => {
