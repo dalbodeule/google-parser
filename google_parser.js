4395abfef35cfe575055d84164828513b5579c3c
small_sunshine <jioo0224@naver.com>
2017-09-10 20:13:09 +0900

    jpg 검색 에러 수정

--- google_parser.js
+++ google_parser.js
@@ -35,6 +35,9 @@ module.exports.jpg = (search) => {
     return new Promise((resolve, reject) => {
         request({
             url: "https://www.google.com/search?q="+encodeURIComponent(search)+"&tbm=isch&ie=UTF-8&tbs=ift:jpg",
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
+            }
         }, (err, res, body) => {
             if(err) reject(err);
             let dom = parser.parseFromString(body);
--- test.js
+++ test.js
@@ -6,7 +6,13 @@ google.search('kirino')
 }).catch((err) => {
     console.log(err);
 });
-google.img('Chitanda Eru')
+google.img('너의 이름은. Another Side: Earthbound')
+.then((res) => {
+    console.log(res[0]);
+}).catch((err) => {
+    console.log(err);
+});
+google.jpg('너의 이름은. Another Side: Earthbound')
 .then((res) => {
     console.log(res[0]);
 }).catch((err) => {
